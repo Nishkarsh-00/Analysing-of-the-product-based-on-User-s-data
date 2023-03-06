043e9c3f5016b70e43abfe46add2ad0e82f232f8
Nishkarsh-00 <75568850+Nishkarsh-00@users.noreply.github.com>
2023-03-06 16:41:51 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -2,11 +2,13 @@ import tweepy
 import pandas as pd
 
 def get_twitter_text(s):
-
-    api_key = 'V4lpiPa5bFuegeZ48n3Cao1zq'
-    api_key_secret = 'ksJwRF0QoJCsFslzgU4zN0utFBrwZu7VaxiFkraGty8t73LJFC'
-    access_token = '1540721724295356416-2QrTq7j8r4R6qSw6oi2bXuScc4RFgn'
-    access_token_secret = 'qoz4UWuerhr302QWnCXp9oM7GhPfo8GoVHNTJeA6zaNfn'
+    
+    #enter your api keys here 
+    # you will be getting these keys from twitter api
+    api_key = ''
+    api_key_secret = ''
+    access_token = ''
+    access_token_secret = ''
 
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
